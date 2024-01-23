ac844155fec2be9ae17aed6eb3f276b5f3b14a80
mhconradt <mhconradt@protonmail.com>
2024-01-22 20:46:13 -0800

    Early exit when there are no paths

--- llfmt/__main__.py
+++ llfmt/__main__.py
@@ -5,6 +5,8 @@ import sys
 def main():
     args = sys.argv[1:]
     paths = [path for pattern in args for path in glob.glob(pattern)]
+    if not paths:
+        return
     paths.sort(key=lambda s: s.split('/'))
     print("\n".join([f"### {path} ###\n{open(path, 'r').read()}" for path in paths]))
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = "2024.1.22"
+VERSION = "2024.1.23"
 
 
 def get_long_description():
